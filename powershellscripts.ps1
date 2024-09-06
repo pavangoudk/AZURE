43955d618312eb09691748852e4e6698299dd56b
k pavan kumar <pavan.kgoud@gmail.com>
2024-09-06 18:44:01 +0530

    Update powershellscripts.ps1

--- powershellscripts.ps1
+++ powershellscripts.ps1
@@ -1,3 +1,37 @@
+#PowerShell Script to number binary 
+
+function Is-BinaryNumber {
+    param (
+        [string]$number
+    )
+    
+    # Initialize a flag to true
+    $isBinary = $true
+
+    # Loop through each character in the string
+    foreach ($char in $number.ToCharArray()) {
+        if ($char -ne '0' -and $char -ne '1') {
+            $isBinary = $false
+            break
+        }
+    }
+
+    # Output the result
+    if ($isBinary) {
+        Write-Output "The number $number is binary."
+    } else {
+        Write-Output "The number $number is not binary."
+    }
+}
+
+# Example usage
+Is-BinaryNumber -number "101010"   # Output: The number 101010 is binary.
+Is-BinaryNumber -number "123456"   # Output: The number 123456 is not binary.
+
+
+##############################################################################################
+
+#PowerShell Script to Swap Two Numbers
 
 # Initialize the numbers
 $a = 5
